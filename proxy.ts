8bf15ad267472d41a12092f529cb29a78eda3d71
naufalhawari001-stack <avirtualzonecom@gmail.com>
2026-08-22 23:42:34 +0700

    okebro

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,8 @@
+// proxy.ts
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
@@ -38,7 +39,7 @@ export async function middleware(request: NextRequest) {
   return response;
 }
 
-// Menentukan rute mana saja yang akan diproses oleh middleware
+// Menentukan rute mana saja yang akan diproses oleh proxy
 export const config = {
   matcher: [
     /*
