0127acf758853b6479413e85e086d9ce6dd5d2ae
naufalhawari001-stack <avirtualzonecom@gmail.com>
2026-08-23 09:12:13 +0700

    studio

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,8 +1,11 @@
+// app/studio/[[...index]]/page.tsx
 'use client';
 
 // 🚀 FIXED: Diimport langsung dari package 'next-sanity' resmi
 import { NextStudio } from 'next-sanity/studio';
 import config from '@/sanity.config'; 
+
+export const dynamic = 'force-dynamic';
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
